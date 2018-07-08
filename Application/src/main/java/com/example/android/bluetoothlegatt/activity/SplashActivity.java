f3622f6589a45099652b9f66b97ec30c16075a1e
wlsl8135 <wlsl8135@naver.com>
2018-07-08 15:30:32 +0900

    Initial commit

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/activity/SplashActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.bluetoothlegatt.activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.example.android.bluetoothlegatt.BuildConfig;
+import com.example.android.bluetoothlegatt.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+public class SplashActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+    private FirebaseRemoteConfig  mFirebaseRemoteConfig;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        linearLayout = (LinearLayout)findViewById(R.id.splashactivity_linearlayout);
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+        mFirebaseRemoteConfig.setDefaults(R.xml.default_config);
+        mFirebaseRemoteConfig.fetch(0)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                        }
+                        display();
+                    }
+                });
+    }
+    void display(){
+        String  splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = mFirebaseRemoteConfig.getString("splash_message");
+        linearLayout.setBackgroundColor(Color.parseColor(splash_background));
+        if(caps){
+            AlertDialog.Builder builder= new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.create().show();
+        }
+        else{
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+    }
+}
